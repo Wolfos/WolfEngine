bb7eda6c8cf6b8d8f113c7ce8f06ac9edae512ea
Robin van Ee <robin.vanee@gmail.com>
2014-09-07 17:20:05 +0200

    Added AngelScript files to the project.

---
+++ Code/Editor/ObjectList.h
@@ -0,0 +1,11 @@
+#ifndef _OBJECTLIST_H
+#define _OBJECTLIST_H
+
+#include "../WolfEngine/WolfEngine.h"
+
+class ObjectList : public Window
+{
+	
+};
+
+#endif
---
+++ Code/WolfEngine/Scripting/README.md
@@ -0,0 +1,2 @@
+This is a fork of AngelScript 2.28.1.
+Current modifications are small but heavier modifications may happen later.
