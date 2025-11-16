d48bfca52a7aa6276ce1702ff64774b6b6f0bcbc
Robin <695652+Wolfos@users.noreply.github.com>
2025-11-16 11:32:18 +0100

    Proper type constraints

--- WolfEngine/ECS/World.cs
+++ WolfEngine/ECS/World.cs
@@ -8,23 +8,23 @@ public class World
 	public Entity CreateEntity() => _entities.Create();
 	public void Destroy(Entity e) => _entities.Destroy(e);
 
-	public void AddComponent<T>(Entity e, in T value = default) where T : struct
+	public void AddComponent<T>(Entity e, in T value = default) where T : struct, IEntityComponent
 		=> Pool<T>().Add(e, value);
 
-	public ref T GetComponent<T>(Entity e) where T : struct
+	public ref T GetComponent<T>(Entity e) where T : struct, IEntityComponent
 		=> ref Pool<T>().Get(e);
 
-	public bool HasComponent<T>(Entity e) where T : struct
+	public bool HasComponent<T>(Entity e) where T : struct, IEntityComponent
 		=> Pool<T>().Has(e);
 
-	public void RemoveComponent<T>(Entity e) where T : struct
+	public void RemoveComponent<T>(Entity e) where T : struct, IEntityComponent
 		=> Pool<T>().Remove(e);
 
 	public View<T1,T2> View<T1,T2>()
-		where T1:struct where T2:struct
+		where T1:struct, IEntityComponent where T2:struct, IEntityComponent
 		=> new(Pool<T1>(), Pool<T2>());
 
-	private ComponentPool<T> Pool<T>() where T:struct
+	private ComponentPool<T> Pool<T>() where T:struct, IEntityComponent
 		=> (ComponentPool<T>) (_pools.TryGetValue(typeof(T), out var p)
 			? p : (_pools[typeof(T)] = new ComponentPool<T>()));
 }
