abcc669dc6b5b10da57f5f14643978b21ed7129f
Robin van Ee <695652+Wolfos@users.noreply.github.com>
2026-03-07 11:56:53 +0100

    Rename

--- WolfEngine.ECS/Attributes.cs
+++ WolfEngine.ECS/Attributes.cs
@@ -6,9 +6,8 @@ public class ExcludeFromEditorAttribute: Attribute
 	
 }
 
-// Doesn't actually do anything yet, but asset cooker will strip these from final build
 [AttributeUsage(AttributeTargets.Struct, Inherited = false)]
-public class EditorOnly: Attribute
+public class EditorOnlyAttribute: Attribute
 {
 	
 }
